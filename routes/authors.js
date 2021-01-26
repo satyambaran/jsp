c16f3fad732c75f54a8390b15cf068f10fea690b
satyambaran <sk915516@gmail.com>
2021-01-26 21:35:48 +0530

    authors show edit delete

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Author = require('../models/author')
 const router = express.Router()
+const Book = require('../models/book')
 router.get('/',async (req,res)=>{
     let searchOptions = {}
     if(req.query.name != null && req.query.name!==""){
@@ -17,7 +18,7 @@ router.get('/',async (req,res)=>{
     }
 })
 
-router.get('/new',(req,res)=>{
+router.get('/new', (req, res) => { //  '/new' can be read as '/:id' that's why we wrote it above
     res.render('authors/new',{author: new Author()})
 })
 /*
@@ -42,18 +43,78 @@ router.post('/',(req,res)=>{
 })*/
 router.post('/',async (req,res)=>{
     const author = new Author({
-        name:req.body.name
+        name: req.body.name
     })
     try {
         const newAuthor = await author.save()
-        //res.redirect('authors/$(newAuthor.id')
-        res.redirect('authors')
+        res.redirect(`authors/${newAuthor.id}`)
+        // res.redirect('authors')
     } catch (error) {
-        res.render('authors/new',{
-            author:author,
-            errorMessage:'Error Creating Author'
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error Creating Author'
         })
     }
 })
-
+router.get('/:id',async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        const books = await Book.find({author:author.id}).limit(8).exec()
+        res.render('authors/show',{
+            author:author,
+            booksByAuthor:books
+        })
+    } catch (error) {
+        //console.log(error)
+        res.redirect('/')
+    }
+})
+router.get('/:id/edit', async (req, res) => {
+    // res.send('Edit' + req.params.id)
+    try {
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit', {
+            author: author
+        })  
+    } catch (error) {
+        res.redirect('/authors')
+    }
+})
+router.put('/:id',async (req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`) //here giving '/' in front means we are giving full url, 
+        //if we don't give this then it means it will be relative url so 'authors/${author.id}' will mean '/authors/authors/${author.id}' i.e. /authors/ will get added
+        // res.redirect('authors')
+    } catch (error) {
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error Updating Author'
+            })
+        }
+    }
+})
+router.delete('/:id',async (req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        await author.remove()
+        res.redirect('/authors') //here giving '/' in front means we are giving full url, 
+        //if we don't give this then it means it will be relative url so 'authors/${author.id}' will mean '/authors/authors/${author.id}' i.e. /authors/ will get added
+        // res.redirect('authors')
+    } catch (error) {
+        //console.log(error)
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.redirect(`/authors/${author.id}`)
+        }
+    }
+})
 module.exports = router
